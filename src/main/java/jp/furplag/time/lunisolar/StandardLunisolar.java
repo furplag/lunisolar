fcb31cff6b6b1335e5fb252ee18d9da9257c5701
furplag <furplag@furplag.jp>
2018-01-04 16:30:01 +0900

    do less complexity .

--- src/main/java/jp/furplag/time/lunisolar/StandardLunisolar.java
+++ src/main/java/jp/furplag/time/lunisolar/StandardLunisolar.java
@@ -15,7 +15,6 @@
  */
 package jp.furplag.time.lunisolar;
 
-import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,12 +23,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
 
-import jp.furplag.time.Julian;
-import jp.furplag.time.Millis;
 import jp.furplag.time.lunisolar.misc.Astror;
 import jp.furplag.time.lunisolar.misc.orrery.EclipticLongitude;
 
@@ -90,62 +86,6 @@ public final class StandardLunisolar extends Lunisolar {
     } while (solarTerms.stream().filter(e -> e.longitude == 315).count() != 3);
 
     return solarTerms;
-  }
-
-  public static void main(String[] args) {
-    List<SolarTerm> solarTerms = Kyoho.termsOfBase(Julian.ofEpochMilli(Instant.parse("2032-12-31T15:00:00.000Z").toEpochMilli()));
-    printr(solarTerms.stream());
-    List<Long> firstDays = Kyoho.termsToFirstDays(solarTerms).stream().map(Millis::ofJulian).collect(Collectors.toList());
-    printr(firstDays.stream().map(Millis::toInstant).map(Kyoho::atOffset));
-
-    List<LunarMonth> monthOfYear = LunarMonth.constructs(solarTerms, firstDays);
-    printr(monthOfYear.stream());
-//
-//    final LunarMonth lastWinterSolstice = monthOfYear.stream().filter(e -> e.november).map(e -> {e.monthOfYear = 11; return e;}).findFirst().orElse(null);
-//    final LunarMonth winterSolstice = monthOfYear.stream().filter(e -> e.november && e.compareTo(lastWinterSolstice) > 0).findFirst().orElse(null);
-//    final ValueRange termRange = ValueRange.of(lastWinterSolstice.range.getMinimum(), winterSolstice.range.getMinimum() - 1);
-//    List<LunarMonth> termTable = monthOfYear.stream().filter(e -> termRange.isValidValue(e.range.getMinimum())).collect(Collectors.toList());
-//    final boolean hasIntercalary = termTable.size() > 12;
-//    final int[] theMonth = {10};
-//    termTable.stream().filter(e -> hasIntercalary && e.intercalaryable).findFirst().ifPresent(e -> e.intercalary = true);
-//    termTable.forEach(e -> {
-//      theMonth[0] += e.intercalary ? 0 : 1;
-//      if (e.monthOfYear == 0) {
-//        e.monthOfYear = (theMonth[0] % 12 == 0 ? 12 : theMonth[0] % 12);
-//      }
-//    });
-//    final LunarMonth nextWinterSolstice = monthOfYear.stream().filter(e -> e.november && e.compareTo(winterSolstice) > 0).findFirst().orElse(null);
-//    final ValueRange nextTermRange = ValueRange.of(winterSolstice.range.getMinimum(), nextWinterSolstice.range.getMinimum() - 1);
-//    List<LunarMonth> nextTermTable = monthOfYear.stream().filter(e -> nextTermRange.isValidValue(e.range.getMinimum())).collect(Collectors.toList());
-//    final boolean hasIntercalaryNext = nextTermTable.size() > 12;
-//    theMonth[0] = 10;
-//    nextTermTable.stream().filter(e -> hasIntercalaryNext && e.intercalaryable).findFirst().ifPresent(e -> e.intercalary = true);
-//    nextTermTable.forEach(e -> {
-//      theMonth[0] += e.intercalary ? 0 : 1;
-//      if (e.monthOfYear == 0) {
-//        e.monthOfYear = (theMonth[0] % 12 == 0 ? 12 : theMonth[0] % 12);
-//      }
-//    });
-//
-////    if (winterSolstice.monthOfYear != 11) {
-////      final LunarMonth nextWinterSolstice = monthOfYear.stream().filter(e -> e.november && e.compareTo(winterSolstice) > 0).findFirst().orElse(null);
-////      final ValueRange nextTermRange = ValueRange.of(winterSolstice.range.getMinimum(), nextWinterSolstice.range.getMaximum());
-////      List<LunarMonth> nextTermTable = monthOfYear.stream().filter(e -> nextTermRange.isValidValue(e.range.getMinimum())).collect(Collectors.toList());
-////    }
-////    System.out.println(winterSolstice);
-//
-//    termTable.addAll(nextTermTable);
-//    final LunarMonth january = termTable.stream().filter(e -> e.monthOfYear == 1).findFirst().orElse(null);
-//    final LunarMonth december = nextTermTable.stream().filter(e -> e.monthOfYear == 12).max(new Comparator<LunarMonth>() {
-//      @Override
-//      public int compare(LunarMonth o1, LunarMonth o2) {
-//        return o1.compareTo(o2);
-//      }}).orElse(null);
-//    printr(termTable.stream().filter(e -> january.range.getMinimum() <= e.range.getMinimum() && e.range.getMinimum() <= december.range.getMaximum()));
-  }
-
-  static void printr(Stream<?> stream) {
-    stream.forEach(System.out::println);
   }
 
   @SuppressWarnings("unchecked")
