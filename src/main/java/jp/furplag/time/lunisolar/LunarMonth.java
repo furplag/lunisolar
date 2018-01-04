ad57f5591de7663383cab68ccfee1920cfc6d0e0
furplag <furplag@furplag.jp>
2018-01-04 11:01:48 +0900

    O_o .

--- src/main/java/jp/furplag/time/lunisolar/LunarMonth.java
+++ src/main/java/jp/furplag/time/lunisolar/LunarMonth.java
@@ -27,23 +27,32 @@ import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import jp.furplag.data.json.Jsonifier;
 import jp.furplag.time.lunisolar.SolarTerm.MidClimate;
 import jp.furplag.time.lunisolar.SolarTerm.PreClimate;
 
 public final class LunarMonth implements Comparable<LunarMonth>, Serializable {
 
+  @JsonProperty
   final ValueRange range;
 
+  @JsonProperty
   final List<SolarTerm> preClimates;
 
+  @JsonProperty
   final List<SolarTerm> midClimates;
 
   final boolean intercalaryable;
 
+  @JsonProperty
   boolean intercalary;
 
   final boolean november;
 
+  @JsonProperty
   int monthOfYear;
 
   LunarMonth(long fromEpochMilli, long toEpochMilli, List<SolarTerm> solarTerms) {
@@ -63,20 +72,6 @@ public final class LunarMonth implements Comparable<LunarMonth>, Serializable {
     // @formatter:on
   }
 
-  private static List<LunarMonth> filtered(final @Nonnull Stream<LunarMonth> stream, Predicate<LunarMonth> predicate) {
-    return stream.filter(predicate).collect(Collectors.toList());
-  }
-
-  @Nullable
-  private static LunarMonth firstNovember(final @Nonnull Stream<LunarMonth> lunarMonths, final LunarMonth start) {
-    return lunarMonths.filter(e -> e.november).filter(e -> (start == null ? Long.MIN_VALUE : start.range.getMinimum()) < e.range.getMinimum()).sorted().findFirst().orElse(null);
-  }
-
-  @Nullable
-  private static LunarMonth firstOf(final @Nonnull List<LunarMonth> lunarMonths, final int monthOfYear) {
-    return lunarMonths.stream().filter(e -> e.monthOfYear == normalize(monthOfYear, 12)).findFirst().orElse(null);
-  }
-
   @Nonnull
   private static List<LunarMonth> intercalaryze(final @Nonnull List<LunarMonth> lunarMonths) {
     final LunarMonth firstOfWinterSolstices = firstNovember(lunarMonths.stream(), null);
@@ -85,6 +80,10 @@ public final class LunarMonth implements Comparable<LunarMonth>, Serializable {
     materialize(lunarMonths, secondOfWinterSolstices.range.getMinimum(), firstNovember(lunarMonths.stream(), secondOfWinterSolstices).range.getMinimum());
 
     return lunarMonths;
+  }
+
+  private static void materialize(final @Nonnull List<LunarMonth> lunarMonths, final long minimum, final long maximum) {
+    materialize(lunarMonths.stream().filter(e -> minimum <= e.range.getMinimum() && e.range.getMinimum() < maximum).sorted().collect(Collectors.toList()));
   }
 
   private static void materialize(final @Nonnull List<LunarMonth> monthsOfWinterSolstice) {
@@ -99,10 +98,6 @@ public final class LunarMonth implements Comparable<LunarMonth>, Serializable {
     });
   }
 
-  private static void materialize(final @Nonnull List<LunarMonth> lunarMonths, final long minimum, final long maximum) {
-    materialize(lunarMonths.stream().filter(e -> minimum <= e.range.getMinimum() && e.range.getMinimum() < maximum).sorted().collect(Collectors.toList()));
-  }
-
   @Nonnull
   private static List<LunarMonth> monthOfYear(final @Nonnull List<LunarMonth> lunarMonths) {
     final LunarMonth january = firstOf(lunarMonths, 1);
@@ -111,12 +106,34 @@ public final class LunarMonth implements Comparable<LunarMonth>, Serializable {
     return filtered(lunarMonths.stream(), e -> range.isValidValue(e.range.getMinimum()));
   }
 
+  @Override
+  public int compareTo(LunarMonth o) {
+    return Long.compare(range.getMinimum(), o.range.getMinimum());
+  }
+
+  @Nullable
+  private static LunarMonth firstNovember(final @Nonnull Stream<LunarMonth> lunarMonths, final LunarMonth start) {
+    return lunarMonths.filter(e -> e.november).filter(e -> (start == null ? Long.MIN_VALUE : start.range.getMinimum()) < e.range.getMinimum()).sorted().findFirst().orElse(null);
+  }
+
+  @Nullable
+  private static LunarMonth firstOf(final @Nonnull List<LunarMonth> lunarMonths, final int monthOfYear) {
+    return lunarMonths.stream().filter(e -> e.monthOfYear == normalize(monthOfYear, 12)).findFirst().orElse(null);
+  }
+
   private static int normalize(final int monthOfYear, final int normalizr) {
     return (monthOfYear % normalizr == 0 ? normalizr : monthOfYear % normalizr);
   }
 
+  private static List<LunarMonth> filtered(final @Nonnull Stream<LunarMonth> stream, Predicate<LunarMonth> predicate) {
+    return stream.filter(predicate).collect(Collectors.toList());
+  }
+
   @Override
-  public int compareTo(LunarMonth o) {
-    return Long.compare(range.getMinimum(), o.range.getMinimum());
+  public String toString() {
+    // @formatter:off
+    try {return Jsonifier.serialize(this);} catch (JsonProcessingException e) {}
+    // @formatter:on
+    return null;
   }
 }
