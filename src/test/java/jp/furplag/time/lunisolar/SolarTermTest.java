db95435a6d37776fa779dba11024cd0c0ad2a0a7
furplag <furplag@furplag.jp>
2018-02-13 11:01:29 +0900

    update dependencies .

--- src/test/java/jp/furplag/time/lunisolar/SolarTermTest.java
+++ src/test/java/jp/furplag/time/lunisolar/SolarTermTest.java
@@ -19,10 +19,17 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 import jp.furplag.time.Julian;
+import jp.furplag.time.Millis;
 
 public class SolarTermTest {
 
@@ -35,4 +42,39 @@ public class SolarTermTest {
     assertThat(SolarTerm.ofClosest(julianDate, 90, Lunisolar.ofJulian(julianDate)).compareTo(SolarTerm.ofClosest(julianDate, 180, Lunisolar.ofJulian(julianDate))), is(-1));
   }
 
+  @Test
+  public void testOfClosest() throws ReflectiveOperationException {
+    final Instant instant = Instant.parse("2033-01-01T00:00:00.000Z");
+    final double springEquinox = Lunisolar.Kyoho.springEquinox(Julian.ofEpochMilli(instant.toEpochMilli()));
+    final double automnEquinox = Lunisolar.Kyoho.closestTerm(Julian.ofEpochMilli(instant.plus(200, ChronoUnit.DAYS).toEpochMilli()), 180);
+    // @formatter:off
+    final Map<OffsetDateTime, SolarTerm> terms = IntStream.range(0, 360)
+      .mapToObj(d->SolarTerm.ofClosest((d < 180 ? springEquinox : automnEquinox), d, Lunisolar.Kyoho))
+      .collect(Collectors.toMap(k -> Lunisolar.Kyoho.atOffset(Millis.toInstant(Lunisolar.Kyoho.asStartOfDay(k.julianDate))), v -> v, (v1, v2) -> v1, LinkedHashMap::new));
+    // @formatter:on
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-03-20T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-04-04T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-04-20T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-05-05T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-05-21T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-06-05T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-06-21T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-07-07T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-07-22T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-08-07T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-08-23T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-09-07T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-09-23T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-10-08T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-10-23T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-11-07T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-11-22T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-12-07T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2033-12-21T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2034-01-05T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2034-01-20T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2034-02-04T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2034-02-18T00:00+09:00")));
+    assertNotNull(terms.get(OffsetDateTime.parse("2034-03-05T00:00+09:00")));
+  }
 }
